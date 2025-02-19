c362f20c1144f084852176863938893ee11ff029
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-19 11:06:35 -0700

    inital data set up

---
+++ app.js
@@ -0,0 +1,48 @@
+const partyAnimals = [
+  {
+    name: 'mike t',
+    emoji: '🦍',
+    partyLocation: 'waterfall'
+  },
+  {
+    name: 'barry',
+    emoji: '🦈',
+    partyLocation: 'reef'
+  },
+  {
+    name: 'bill',
+    emoji: '🦢',
+    partyLocation: 'wetlands'
+  },
+  {
+    name: 'remy',
+    emoji: '🐒',
+    partyLocation: 'waterfall'
+  },
+  {
+    name: 'road runner',
+    emoji: '🐦',
+    partyLocation: 'desert'
+  },
+  {
+    name: 'roman',
+    emoji: '🐍',
+    partyLocation: 'desert'
+  },
+  {
+    name: 'lucille',
+    emoji: '🦭',
+    partyLocation: 'reef'
+  },
+  {
+    name: 'steve',
+    emoji: '🐉',
+    partyLocation: 'desert'
+  },
+  {
+    name: 'doug',
+    emoji: '🦎',
+    partyLocation: 'desert'
+  },
+
+]
